1e378211174cfae1c4248410ca3a0e4104bd2953
Jacob Ruiz <jacobruiz100@yahoo.com>
2019-09-18 15:07:02 -0500

    Adding

--- Session 4/src/Balance.java
+++ Session 4/src/Balance.java
@@ -1,14 +1,7 @@
-public class Balance
-{
-    public static void main(String[] args)
-    {
-        int balance = 100;
-        int amount = 0;
-        String text = " ";
-        while ( balance > 0 )
-            (
-                text = "Your balance is: \u0024" + balance + "*";
-                text += "\nAmount to withdraw";
-                )
+public class Balance {
+
+    public static void main(String[] args) {
+
+
     }
 }
